f1f8a5199657800d7fc2245c704571278590f1ce
surajbridgelabz <surajnsinha@gmail.com>
2022-11-21 15:21:20 +0530

    [Suraj] :week days

--- SelectionIf&else.js
+++ SelectionIf&else.js
@@ -26,18 +26,46 @@
 //       console.log("the Third number is greatest among all");
 //   }
 
-//UC2
-let year=1980;
-if (year % 400 == 0)
-{
-  console.log("year is leap year");
-}
-else if (year % 4 == 0)
-{
-  console.log("year is leap year");
-}
-else
-{
-  console.log("year is not leap year");
-}
+// //UC2
+// let year=1980;
+// if (year % 400 == 0)
+// {
+//   console.log("year is leap year");
+// }
+// else if (year % 4 == 0)
+// {
+//   console.log("year is leap year");
+// }
+// else
+// {
+//   console.log("year is not leap year");
+// }
   
+let max=7;
+let min=1;
+ let checknum=Math.floor(Math.random() * (max - min + 1) + min);
+ console.log(checknum);
+switch(checknum)
+{
+  case 1:
+    console.log("TODAY is-MONDAY");
+    break;
+  case 2:
+    console.log("TODAY is-TUESDAY");
+    break;
+  case 3:
+    console.log("TODAY is-WEDNESDAY");
+    break;
+  case 4:
+    console.log("TODAY is-THURSDAY");
+    break;
+  case 5:
+    console.log("TODAY isFRIDAY");
+    break;
+  case 6:
+    console.log("TODAY is-SATURDAY");
+    break;
+  case 7:
+    console.log("TODAY is-SUNDAY");
+    break;
+}
